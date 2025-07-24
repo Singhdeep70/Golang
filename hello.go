2bdae32fde2edf38130e8736170bd560a7699583
Harshdeep Singh <148795958+Singhdeep70@users.noreply.github.com>
2025-07-25 00:14:08 +0530

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,8 +1,11 @@
+// Package main defines the entry point of the Go program
 package main
 
+// Importing the fmt package for formatted I/O operations (e.g., printing to the console)
 import "fmt"
 
-
-func main(){
+// The main function is the starting point of every Go program
+func main() {
+	// Print a greeting message to the console
 	fmt.printf("HELLO THERE!")
 }
