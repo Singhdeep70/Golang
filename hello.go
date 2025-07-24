295b8890d902fc7f84a780bb2c737d5493683e6d
singhdeep70 <hs74519252@gmail.com>
2025-07-25 00:04:48 +0530

    Initial commit

---
+++ hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	fmt.printf("HELLO THERE!")
+}
---
+++ variables.go
@@ -0,0 +1,21 @@
+package main
+
+
+import "fmt"
+
+
+func main(){
+	
+	var zeroInt int // 0
+
+	var zeroFloat float64 // 0.00
+
+	var zeroBool bool // false
+
+	var zeroString string // ""
+
+
+	fmt.printf("%v %.2f %v %q\n", zeroInt, zeroFloat, zeroBool, zeroString)
+
+
+}
